67c5db026308da669d2d28a22a7028fdb6ada7fd
Azizbek Komilov <komilovazizbek2562@gmail.com>
2026-01-21 16:15:56 +0500

    Add files via upload

---
+++ js.19/js.js
@@ -0,0 +1,69 @@
+// // string
+// console.log('Hello world fuihv27246 !@#$%^&');
+
+
+// let email = 'komilovazizbek4385@gmail.com'
+// console.log(email);
+
+
+// // string concatenation
+
+// let firstName = 'Aziz'
+// let lastName = 'Komilov'
+// let  fullName = firstName + ' ' + lastName
+// console.log(fullName);
+
+// // getting choracters
+
+// console.log(fullName[2]);
+
+// //string length => uzunlik
+
+// console.log(fullName.length);
+
+
+
+// // string methods => funksiya o'xshash ish bajaradi
+
+// console.log(fullName.toUpperCase());
+
+
+
+
+// Kop ishlatiladigan metodlar
+
+
+let email = ' Komilovazizbek@gamil.com '
+
+// LastIndeOf()
+// let result = email.lastIndexOf('@')
+// console.log(result);
+// indexOf 
+
+// let result = email.IndexOf('@')
+// console.log(result);
+
+
+// slice()
+// let result = email.slice(10)
+// console.log(result);
+
+//substr()
+// let result = email.substr(1,4)
+// console.log(result);
+
+//replace()
+// let result = email.replace('k', '@')
+// console.log(result);
+
+// charAt()
+// let result = email.charAt()
+// console.log(result);
+// toUpperCase
+
+// let result = email.toLocaleLowerCase()
+// console.log(result);
+// treim()
+let result = email.trim()
+console.log(result.length, email.length);
+
